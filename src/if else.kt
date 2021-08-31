9954274c64e3e2a7bf98c43de9d6529c6f2611fc
GuerraAna <ana.guerrads@hotmail.com>
2021-08-30 23:14:55 -0300

    If else simples

---
+++ src/if else.kt
@@ -0,0 +1,15 @@
+fun main() {
+    maiorDeIdade(21)
+    maiorDeIdade(12)
+    maiorDeIdade(45)
+}
+
+fun maiorDeIdade(idade: Int) {
+
+    if(idade >= 18) {
+        println("Maior de idade")
+    } else {
+        println("Menor de idade")
+    }
+
+}

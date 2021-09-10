e7c4fd024bbf65160423a381ae619747c98e5fc8
GuerraAna <ana.guerrads@hotmail.com>
2021-09-09 23:23:04 -0300

    With

---
+++ src/Poo/With.kt
@@ -0,0 +1,28 @@
+package Poo
+
+class Computador() {
+
+    fun liga() {
+        
+    }
+
+    fun processa() {
+
+    }
+
+    fun desliga() {
+
+    }
+}
+
+fun main() {
+
+    val notebook = Computador()
+
+    with(notebook) {
+        liga()
+        processa()
+        desliga()
+    }
+
+}
